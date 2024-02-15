d84fcfc60d92deda492e88072efa08f06ee01756
조미혜 <mihye0924@freeive.com>
2024-02-15 19:51:23 +0900

    [02/15] detail 레이아웃 수정

--- client/src/pages/DetailPage.tsx
+++ client/src/pages/DetailPage.tsx
@@ -1,103 +1,153 @@
 import Accordion from "@/components/Accordion"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styled from "styled-components"
 
+interface StepType {
+    id: number,
+    title: string,
+    active: boolean
+}
 const DetailPage = () =>  {
-    const [accordionButton, setAccordionButton] = useState(false)
+    const [active, setActive] = useState(0)
+    const [toggle, setToggle] = useState(false)
+    const [step, setStep] = useState<StepType[]>([]) 
+ 
+    useEffect(() => {
+        setStep([
+            {
+                id: 0,
+                title: "후원내용",
+                active: false
+            },
+            {
+                id: 1,
+                title: "STEP1. 후원분야 선택",
+                active: false
+            },
+            {
+                id: 2,
+                title: "STEP2. 후원자 정보",
+                active: false
+            },
+            {
+                id: 3,
+                title: "STEP3. 후원금 납입",
+                active: false
+            },
+            {
+                id: 4,
+                title: "STEP4. 신청완료",
+                active: false
+            }
+        ])
+    },[])
+ 
     return(
         <Article>
-            <ArticleInner>
+            <ArticleInner image={'image01.jpg'}>
                 <ContentWrap> 
                     <aside>
                         <h1>후원신청</h1>
                         <p>낚싯줄에 걸린 돌고래 '종달이'를 구해주세요</p>
                     </aside>
                     <section>
-                        <Accordion  
-                            active={true}
-                            title="후원내용"
-                        >
-                            <>
-                                1월 29일 제주돌고래 긴급구조단이 종달이 꼬리에 걸린 낚싯줄 제거에 성공했습니다. 아직 종달이 몸과 입에 제거하지 못한 낚싯줄이 남아 있습니다
-                            </>
-                        </Accordion>
-                        <Accordion 
-                            toggle
-                            title="STEP 1. 후원분야 선택"
-                            active={accordionButton} 
-                            onClick={() => setAccordionButton(!accordionButton)}
-                        >
-                            <>내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용</>
-                        </Accordion>
-                        <Accordion 
-                            toggle
-                            title="STEP 1. 후원분야 선택"
-                            active={accordionButton} 
-                            onClick={() => setAccordionButton(!accordionButton)}
-                        >
-                            <>내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용</>
-                        </Accordion>
-                        <Accordion 
-                            toggle
-                            title="STEP 1. 후원분야 선택"
-                            active={accordionButton} 
-                            onClick={() => setAccordionButton(!accordionButton)}
-                        >
-                            <>내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용</>
-                        </Accordion>
-                        <Accordion 
-                            toggle
-                            title="STEP 1. 후원분야 선택"
-                            active={accordionButton} 
-                            onClick={() => setAccordionButton(!accordionButton)}
-                        >
-                            <>내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용</>
-                        </Accordion>
+                        {
+                            step.map((item)=> {
+                                return(
+                                    <Accordion   
+                                        key={item.id}
+                                        title={item.title}
+                                        active={
+                                            item.id === 0 || item.id === 1 ? true : false || 
+                                            toggle && item.id === active || item.active ? true : false
+                                            }
+                                        toggle={item.id !== 0 ? true : false}
+                                        onClick={() => {
+                                            setActive(item.id),
+                                            setToggle(item.active = !item.active)  
+                                        }}
+                                    >
+                                    {(() => {
+                                    switch (item.id) {
+                                    case 0:
+                                        return <>1월 29일 제주돌고래 긴급구조단이 종달이 꼬리에 걸린 낚싯줄 제거에 성공했습니다. 아직 종달이 몸과 입에 제거하지 못한 낚싯줄이 남아 있습니다</>
+                                    case 1:
+                                        return<>후원분야</>  
+                                    case 2:
+                                        return<>후원자정보</>  
+                                    case 3:
+                                        return<>후원금납입</>  
+                                    case 4:
+                                        return<>친청완료</>  
+                                    default:
+                                        return null;
+                                    }
+                                })()}
+
+                                    </Accordion> 
+                                )
+                            })
+                        }
+                       
                     </section>
                 </ContentWrap>
-            </ArticleInner>
-            <ImageWrap>
-                <img src={`${import.meta.env.VITE_SERVER_URL}/uploads/donation/image01.jpg`} alt="배경이미지"/>
-            </ImageWrap>
+            </ArticleInner>  
         </Article>
     )
 }
 export default DetailPage
+const sizes = {
+    desktop: "1024px",
+    tablet: "768px",
+    mobile: "375px"
+  };
+  
+  // 미디어 쿼리를 위한 도우미 함수
+  const media = {
+    desktop: `(max-width: ${sizes.desktop})`,
+    tablet: `(max-width: ${sizes.tablet})`,
+    mobile: `(max-width: ${sizes.mobile})`,
+  };
 
+  
 const Article = styled.article`
-    position: relative;  
-    /* height: calc(100vh - 207px); */
-    /* padding-top: 148px; */
-    margin-top: 148px;
-    /* height: calc(100vh - 57px); */
-    overflow-y: auto;
-    @media only screen and (max-width: 375px) {  
-        padding-top: 115px;
+    position: relative;   
+    margin-top: 148px;  
+    @media ${media.mobile}{  
+        margin-top: 110px;
     }
 `
 
-const ArticleInner = styled.div`
+const ArticleInner = styled.div<{image?:string}>`
     position: relative;
-    max-width: 1140px;
-    width: 100%;
-    margin: 0 auto;
+    height: 100%;  
+    background: url(${(props) => `${import.meta.env.VITE_SERVER_URL}`+'/uploads/donation/'+props.image}) no-repeat top center;
+    background-size: 100% 100%;
+    background-attachment: fixed;
+    @media ${media.mobile}{  
+        background-size: 100% 300px;
+        background-attachment: initial;
+    }
 `
 const ContentWrap = styled.div` 
-    position: absolute; 
+    max-width:${sizes.desktop};
+    width: 100%; 
     height: auto; 
     display: flex;
-    gap: 50px;
-    z-index: 2;
+    flex-wrap: wrap;
+    gap: 50px; 
+    margin: 0 auto;
     padding: 50px 10px;
     aside {
         flex: 1 0 20%;
-        border: 1px solid red;
+        /* border: 1px solid red; */
         h1 {
+            font-family: 'NanumSquareNeo-Variable';
             font-size: 32px;
             color: #fff;
         }
         p {
-            font-size: 20px;
+            font-size: 18px;
             color: #fff;
             line-height: 1.5;
             margin-top: 20px;
@@ -105,25 +155,34 @@ const ContentWrap = styled.div`
     }
     section {
         flex: 1 0 70%;
-        border: 1px solid red;
+        /* border: 1px solid red; */
         >div { 
             &:not(:first-child) {
                 margin-top: 20px;
             }
         }
     }
-`
-const ImageWrap = styled.div` 
-    background-color: rgba(0, 0, 0, 0.33);
-    position:relative;
-    width: 100%;
-    height: 100%;
-    /* height: calc(100vh - 207px); */
-    pointer-events: none;
-    img { 
-        object-fit: cover;
-        width: 100%;
-        height: 100%;
-        z-index: -1;
+    @media ${media.mobile}{  
+        gap: 0;
+        flex-direction: column; 
+        section {
+            margin-top: 100px;
+            >div {
+                border: 1px solid #f1f1f1;
+                border-radius: 15px;
+            }
+        }
+        aside { 
+            flex-basis: 195px;
+            justify-content: center;
+            display: inline-flex;
+            flex-direction: column;
+            h1 {
+                font-size: 24px;
+            }
+            p {
+                font-size: 16px;
+            }
+        }
     }
-`
+` 
