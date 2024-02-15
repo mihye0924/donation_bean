9b1c01c8f6fe2239932b0554b1050697ac8504e1
Jiwon Park <139869491+whereisjw@users.noreply.github.com>
2024-02-15 15:20:46 +0900

    [02/15] 라우터수정

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MainPage from "@/pages/MainPage";
 import SignInPage from "@/pages/SignInPage";
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/signin" element={<SignInPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/signin" element={<SignInPage />} />
+    </Routes>
   );
 };
 export default Router;
