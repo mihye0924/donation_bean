ccbde7f732dc7bdcff7f4a76914954ffa6254433
whereisjw <j9399977@gmail.com>
2024-02-20 15:09:10 +0900

    [02/20] 아이디기능임시구현

--- server/router/UserRouter.js
+++ server/router/UserRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { insertUser } from "../controller/UserController.js";
+import { getId, insertUser, login } from "../controller/UserController.js";
 
 const router = express.Router();
 
 router.post(`/signin`, insertUser);
+router.get(`/signin`, getId);
+router.post(`/login`, login);
 
 export default router;
