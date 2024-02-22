81e6bfa4def93d2faf5796f827f84287d9a1c40f
FEEL-2012309\user <dudwk9946@naver.com>
2024-02-22 15:25:02 +0900

    [2024.02.22] swiper 추가

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,46 +1,47 @@
 import CardList from "@/components/CardLise"
 import Radio from "@/components/Radio"
 import Select from "@/components/Select"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styled from "styled-components"
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import ReactDOMServer from 'react-dom/server';
+import 'swiper/css';
+import 'swiper/css/pagination';
+// Radio
 const RadioList = [
     {   
-        keyId: 0,
         label: '전체1',
         id: 'option1',
         value: 'option1',
         imgUrl: 'img'
     },
     {
-        keyId: 1,
         label: '전체1',
         id: 'option2',
         value: 'option2',
         imgUrl: 'img'
     },
     {
-        keyId: 2,
         label: '전체1',
         id: 'option3',
         value: 'option3',
         imgUrl: 'img'
     },
     {
-        keyId: 3,
         label: '전체1',
         id: 'option4',
         value: 'option4',
         imgUrl: 'img'
     },
     {
-        keyId: 4,
         label: '전체1',
         id: 'option5',
         value: 'option5',
         imgUrl: 'img'
     }
 ]
+// Card
 const CardItemList = [
     {
         id: 0,
@@ -54,7 +55,7 @@ const CardItemList = [
         percentage: 50
     },
     {
-        id: 0,
+        id: 1,
         to:"/",
         imgSrc:"./", 
         imgUrl:"./" ,
@@ -65,7 +66,7 @@ const CardItemList = [
         percentage: 50
     },
     {
-        id: 0,
+        id: 2,
         to:"/",
         imgSrc:"./", 
         imgUrl:"./" ,
@@ -76,7 +77,7 @@ const CardItemList = [
         percentage: 50
     },
     {
-        id: 0,
+        id: 3,
         to:"/",
         imgSrc:"./", 
         imgUrl:"./" ,
@@ -87,7 +88,7 @@ const CardItemList = [
         percentage: 50
     },
     {
-        id: 0,
+        id: 4,
         to:"/",
         imgSrc:"./", 
         imgUrl:"./" ,
@@ -98,7 +99,7 @@ const CardItemList = [
         percentage: 50
     },
     {
-        id: 0,
+        id: 5,
         to:"/",
         imgSrc:"./", 
         imgUrl:"./" ,
@@ -109,7 +110,7 @@ const CardItemList = [
         percentage: 50
     },
     {
-        id: 0,
+        id: 6,
         to:"/",
         imgSrc:"./", 
         imgUrl:"./" ,
@@ -120,6 +121,7 @@ const CardItemList = [
         percentage: 50
     }
 ]
+// Select
 const selectOptions = [
     {
         value: "123",
@@ -130,16 +132,60 @@ const selectOptions = [
         label: "13"
     }
 ]
+
+// Slide
+const SlideList = [
+    {
+        id: 0,
+        img: '/images/slideBanner1.png'
+    },
+    {
+        id: 1,
+        img: '/images/slideBanner2.png'
+    },
+    {
+        id: 2,
+        img: '/images/slideBanner3.png'
+    },
+    {
+        id: 3,
+        img: '/images/slideBanner4.png'
+    }
+]
 const MainPage = () =>  {
+    // Radio Index
     const [radioActive, setRadioActive] = useState<number>();
+    // Swiper slide 영역
+    const [swiperIndex, setSwiperIndex] = useState<number>(0); // -> 페이지네이션용
+    const onSwiper = (e:any) => {
+        console.log(e)
+        setSwiperIndex(e.realIndex + 1);
+    }
+    const renderProgressbar = (progressbarFillClass:string) => {
+        return ReactDOMServer.renderToStaticMarkup(<span className={progressbarFillClass}>{swiperIndex}</span>);
+    }
     return(
         <MainInner>
-            {/* 슬라이드 확정시 변경 */}
-            <SlideList>
-                <div>
+            {/* 슬라이드 확정시 변경 */}{swiperIndex}
+            <SwiperWrap>
+                <Swiper
+                    pagination={{clickable: true, type: 'progressbar', renderProgressbar}}
+                    modules={[Pagination]}
+                    loop
+                    onSlideChange={(e) => onSwiper(e)}
+                    className="mySwiper"
+                    >
+                        {
+                            SlideList.map((item) => {
+                                return (
+                                    <SwiperSlide key={item.id}><img src={item.img} alt="배너" />${swiperIndex}</SwiperSlide>
+                                )
+                            })
+                        }
+                    <SwiperSlide><img src="/images/slideBanner1.png" alt="배너"/></SwiperSlide>
 
-                </div>
-            </SlideList>
+                </Swiper>
+            </SwiperWrap>
             <SelectWrap>
                 <Select 
                     selectOptions={selectOptions}
@@ -152,8 +198,8 @@ const MainPage = () =>  {
                         RadioList.map((item, index) => {
                             return (
                                 <Radio 
+                                    key={item.id}
                                     className={radioActive === index ? "active" : ""}
-                                    key={item.keyId}
                                     label={item.label}
                                     id={item.id} 
                                     value={item.value} 
@@ -190,18 +236,56 @@ const MainPage = () =>  {
     )
 }
 export default MainPage 
+const sizes = {
+    desktop: "1200px",
+    tablet: "768px",
+    mobile: "375px"
+}; 
+const media = {
+    desktop: `(max-width: ${sizes.desktop})`,
+    tablet: `(max-width: ${sizes.tablet})`,
+    mobile: `(max-width: ${sizes.mobile})`,
+    }; 
 
 const MainInner = styled.div`
     max-width: 1200px;
     width: 100%;
     margin: 0 auto;
+    padding-top: 147px;
 `
-const SlideList = styled.div`
+const SwiperWrap = styled.div`
     height: 500px;
-    background-color: #ddd;
+    .mySwiper {
+        height: 100%;
+        .swiper-slide {
+            height: 100%;
+            img {
+                width: 100%;
+                height: 100%;
+            }
+        }
+        @media ${media.tablet}{  
+            .swiper-slide {
+                height: 307px;
+                img {
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+        }
+        @media ${media.mobile}{  
+            .swiper-slide {
+                height: 150px;
+                img {
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+        }
+    }
 `
 const SelectWrap = styled.div`
-    background-color: #ddd;
+
 `
 
 const RadioWrap = styled.div`
